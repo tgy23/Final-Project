0ae2ac40361d34f9e2162573a098e12769294303
Tgy Gettu <tgynoa@yahoo.com>
2025-04-26 22:07:28 +0200

    Create main.tf

---
+++ Code/IaC-with-terraform/main.tf
@@ -0,0 +1,55 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "novi-education"
+  location = "North Europe"
+}
+
+# Create a Storage Account
+resource "azurerm_storage_account" "st" {
+  name                     = "noviprojectst"  
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+}
+
+# Create Blob Storage Containers
+resource "azurerm_storage_container" "incoming" {
+  name                  = "incoming"
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "cleaned" {
+  name                  = "cleaned"
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "curated" {
+  name                  = "curated"
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
+}
+
+# Create an Azure Databricks Workspace
+resource "azurerm_databricks_workspace" "dbx" {
+  name                = "novidbxworkspace"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                 = "premium"
+}
+
+
+
+# Create an Azure Data Factory
+resource "azurerm_data_factory" "adf" {
+  name                = "novidatafactory"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+}
+
